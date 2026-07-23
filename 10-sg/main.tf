7445997f0ada4d553f2be905308d7c88e9c9fcd3
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-07-23 22:37:22 +0530

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,6 +1,7 @@
 module "catalogue" {
     source = "terraform-aws-module/security-group-aws"
     name = "${local.common_name_suffix}-catalogue"
+    use_name_prefix = false
     description = "Security group for catalogue with custom ports open with vpc, egress all traffic"
     vpc_id = data.aws_ssm.parameter.vpc_id.value
 }
