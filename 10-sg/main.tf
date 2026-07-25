8fda1d0b1a72c5ccf9090b3406fc8141856fe97a
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-07-25 23:11:04 +0530

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,7 +1,18 @@
-module "catalogue" {
-    source = "terraform-aws-module/security-group-aws"
-    name = "${local.common_name_suffix}-catalogue"
-    use_name_prefix = false
-    description = "Security group for catalogue with custom ports open with vpc, egress all traffic"
-    vpc_id = data.aws_ssm.parameter.vpc_id.value
+# Using open source module
+
+#module "catalogue" {
+#    source = "terraform-aws-module/security-group-aws"
+#    name = "${local.common_name_suffix}-catalogue"
+#    use_name_prefix = false
+#    description = "Security group for catalogue with custom ports open with vpc, egress all traffic"
+#    vpc_id = data.aws_ssm.parameter.vpc_id.value
+#}
+
+module "sg" {
+    source = "git::https://github.com/GuddapathalaVineela/terraform-aws-sg.git?ref=main"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "mongodb"
+    sg_description = "Created for mongodb"
+    vpc_id = local.vpc_id
 }
