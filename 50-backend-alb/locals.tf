3098b42f5c3b8ca73380e94825ce90a4b5a3e3f6
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-07-28 23:18:19 +0530

    terraform

--- 50-backend-alb/locals.tf
+++ 50-backend-alb/locals.tf
@@ -9,3 +9,19 @@ locals {
         Terraform = "true"
     }
 }
+
+#Backend ALB listening on port number 80
+resource "aws_lb_listener" "backend_alb" {
+    load_balancer_arn = aws_lb.backend_alb.arn
+    port = "80"
+    protocol = "HTTP"
+    default_action {
+        type = "fixed-response"
+
+    fixed-response {
+        content_type = "text/plain"
+        message_body = "Hi, I am from backend ALB HTTP"
+        status_code = "200"
+    }
+    }
+}
