4af98e59e9ab726c814b5dd4cac0adc820de28b8
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-07-30 22:51:35 +0530

    terraform

--- 30-sg-rules/locals.tf
+++ 30-sg-rules/locals.tf
@@ -1,4 +1,5 @@
 locals {
     backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
     bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+    mongo_sg_id = data.aws_ssm_parameter.mongo_sg_id.value
 }
