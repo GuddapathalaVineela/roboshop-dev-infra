f8bfbd62f874dd20df4f662922bbd6a35ff59664
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-07-30 22:49:42 +0530

    terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -8,7 +8,25 @@ resource "aws_instance" "mongodb" {
         local.common_tags,
         {
             Name = "${local.common_name_suffix}-mongodb" #roboshop-dev-mongodb
-            
+
         }
     )
+
+resource "terraform_data" "mongodb" {
+    triggers_replace = [
+        aws_instance.mongodb.id
+    ]
+    connection {
+        type = "ssh"
+        user = "ec2-user"
+        password = "DevOps321"
+        host = aws_instance.mongodb.private_ip
+    }
+
+    provisioner "remote_exec" {
+        inline = [
+            "echo Hello World"
+        ]
+    }
+}
 }
