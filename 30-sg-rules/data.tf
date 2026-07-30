b04e46990d493175ee64967f74035b12001ed5e2
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-07-30 22:51:04 +0530

    terraform

--- 30-sg-rules/data.tf
+++ 30-sg-rules/data.tf
@@ -9,3 +9,7 @@ data "aws_ssm_parameter" "bastion_sg_id" {
 data "aws_ssm_parameter" "private_subnet_ids" {
     name = "/${var.project_name}/${var.environment}/private_subnet_ids"
 }
+
+data "aws_ssm_parameter" "mongo_sg_id" {
+    name = "/${var.project_name}/${var.environment}/mongo_sg_id"
+}
