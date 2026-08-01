4a9bbce7ee6119061abbe67ca660604c58db7df4
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-08-01 22:56:11 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -2,4 +2,4 @@
 component = $1
 
 dnf install ansible -y
-ansible-pull -U https://github.com/GuddapathalaVineela/ansible-roboshop-roles-tf.git -e component = mongodb main.yaml
+#ansible-pull -U https://github.com/GuddapathalaVineela/ansible-roboshop-roles-tf.git -e component = mongodb main.yaml
