d76adc1d0de13ac042d5b68c7349e063ad8c5cdd
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-08-07 23:07:46 +0530

    terraform

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "domain_name" {
+    default = "daws86s.co.in"
+}
+
+variable "zone_id" {
+    default = ""
+}
