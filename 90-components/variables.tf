81de01b9ad3fbfd3ae8d51dad74a0cb1800cd0b2
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-08-12 22:23:52 +0530

    terraform

--- 90-components/variables.tf
+++ 90-components/variables.tf
@@ -9,3 +9,31 @@ variable "rule_priority" {
 variable "domain_name" {
     default = "daws86s.co.in"
 }
+
+variable "components" {
+    default = {
+        catalogue = {
+            rule_priority = 10
+        }
+
+        user = {
+            rule_priority = 20
+        }
+
+        cart = {
+            rule_priority = 30
+        }
+
+        shipping = {
+            rule_priority = 40
+        }
+
+        payment = {
+            rule_priority = 50
+        }
+
+        frontend = {
+            rule_priority = 10
+        }
+    }
+}
