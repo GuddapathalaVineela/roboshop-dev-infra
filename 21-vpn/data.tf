e424f368e30b4eda8dceb80c57372a02ddb90e28
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-08-15 22:34:25 +0530

    terraform

--- 21-vpn/data.tf
+++ 21-vpn/data.tf
@@ -17,6 +17,6 @@ data "aws_ami" "joindevops" {
     }
 }
 
-data "aws_ssm_parameter" "bastion_sg_id" {
-    name = "/${var.project_name}/${var.environment}/bastion_sg_id"
+data "aws_ssm_parameter" "open_vpn_sg_id" {
+    name = "/${var.project_name}/${var.environment}/open_vpn_sg_id"
 }
