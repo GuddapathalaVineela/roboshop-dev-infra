4398a2493ec6045d1ab08147c9244302ea5a14a3
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-08-15 22:36:54 +0530

    terraform

--- 21-vpn/locals.tf
+++ 21-vpn/locals.tf
@@ -1,6 +1,6 @@
 locals {
     ami_id = data.aws_ami.joindevops.id
-    bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+    open_vpn_sg_id = data.aws_ssm_parameter.open_vpn_sg_id.value
     public_subnet_ids = split(",",data.aws_ssm_parameter.public_subnet_ids.value)[0]
 
     common_tags = {
