7fe151e819fcd0929b26adf4f8c77096ca278fc8
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-08-15 22:38:08 +0530

    terraform

--- 21-vpn/main.tf
+++ 21-vpn/main.tf
@@ -1,26 +1,23 @@
-resource "aws_instance" "bastion" {
+resource "aws_instance" "openvpn" {
     ami = local.ami_id
     instance_type = "t3.micro"
-    vpc_security_group_ids = [local.bastion_sg_id]
+    vpc_security_group_ids = [local.openvpn_sg_id]
     subnet_id = local.public_subnet_id
-    iam_instance_profile = aws_iam_instance_profile.bastion.name
-     # need more for terraform
-    root_block_device {
-        volume_size = 50
-        volume_type = "gp3" # or "gp2", depending on your preference
-    }
-
-
-    user_data = file("bastion.sh")
+    user_data = file("vpn.sh")
+    
     tags = merge (
         local.common_tags,
-    {
-       Name = "${var.project_name}-${var.environment}-bastion"
-    }
-   ) 
+        {
+            Name = "${var.project_name}-${var.environment}-openvpn"
+        }
+    )
 }
 
-resource "aws_iam_instance_profile" "bastion" {
-    name = "bastion"
-    role = "BastionTerraformAdmin"
+resource "aws_route53_record" "openvpn" {
+  zone_id = var.zone_id
+  name    = "openvpn.${var.domain_name}" # openvpn.daws86s.fun
+  type    = "A"
+  ttl     = 1
+  records = [aws_instance.openvpn.public_ip]
+  allow_overwrite = true
 }
