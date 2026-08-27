e8692ede5c06b921e320c79a7106318fe081bc53
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-08-27 23:12:17 +0530

    terraform

--- 91-cloudfront/locals.tf
+++ 91-cloudfront/locals.tf
@@ -1,6 +1,5 @@
 locals {
     cachingOptimised = data.aws_cloudfront_cache_policy.cachingOptimised.id
-    cachingOptimised = data.aws_cloudfront_cache_policy.cachingOptimised.id
     cachingDisabled = data.aws_cloudfront_cache_policy.cachingDisabled.id
     cdn_certificate_arn = data.aws_ssm_parameter.certificate_arn.value
     common_tags = { 
