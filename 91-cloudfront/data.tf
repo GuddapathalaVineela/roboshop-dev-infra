a5cf3cdc533de7fbfb9e0839e96a58e85fc1dd5a
GuddapathalaVineela <guddapathalavineela15@gmail.com>
2026-08-28 23:36:15 +0530

    terraform

--- 91-cloudfront/data.tf
+++ 91-cloudfront/data.tf
@@ -11,6 +11,5 @@ data "aws_ssm_parameter" "certificate_arn" {
   name = "/${var.project_name}/${var.environment}/frontend_alb_certificate_arn"
 }
 
-# *.daws86s.fun
 data "aws_ssm_parameter" "certificate_arn" {
 }
